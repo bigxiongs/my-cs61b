bb3e3accb4d334eca63304477726b82cb986692e
xiong-deshan <deshan-xiong@outlook.com>
2023-07-07 09:08:55 +0800

    unknown modification

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,27 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import javax.swing.text.Position;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static void addRow(TETile[][] world,
+                               int x, int y, int s, TETile t) {
+        for (int i = y; i < s; i++) {
+            world[x][i] = t;
+        }
+    }
+    public static void addHexagon(TETile[][] world,
+                                  int x, int y, int s, TETile t) {
+        assert s > 2 : "require size > 2 but " + s + " given.";
+        assert x + 2 * s - 1 < world.length;
+        assert y + 2 * s - 1 < world[0].length;
+        for (int i = 0; i < s; i++) {
+            addRow(world, x + i, y + s, s + i, t);
+            addRow(world, x + 2 * s - 1 - i, y + s, s + i, t);
+        }
+    }
 }
